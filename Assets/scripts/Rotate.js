e3c84567058f2e374001bb5e57e32d6497ef6019
fernando-carrillo <fernando.carrillo@onlive.com>
2015-01-25 01:52:45 -0800

    Added code for rotating the world

---
+++ Assets/scripts/Rotate.js
@@ -0,0 +1,15 @@
+﻿ #pragma strict
+ 
+ private var qStart : Quaternion;
+ 
+ function OnMouseDown() {
+     var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+     var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+     qStart = Quaternion.Inverse(Quaternion.AngleAxis(angle, Vector3.forward)) * transform.rotation;
+ } 
+ 
+ function OnMouseDrag() {
+     var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+     var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+     transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward) * qStart;
+ }
---
+++ Assets/scripts/RotateWithMouse.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateWithMouse : MonoBehaviour {
+
+	public float clicksPer360 = 12;
+	private float snapDegreesAtEvery;
+	private Quaternion qStart;
+	private float prevSnappedAngle;
+
+	void Start(){
+		snapDegreesAtEvery = 360/clicksPer360;
+		prevSnappedAngle = 0;
+	}
+	
+	void OnMouseDown() {
+		Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		qStart = Quaternion.Inverse(Quaternion.AngleAxis(calculateSnap(angle), Vector3.forward)) * transform.rotation;
+	} 
+	
+	void OnMouseDrag() {
+		Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		float snappedAngle = calculateSnap (angle);
+		transform.rotation = Quaternion.AngleAxis(snappedAngle, Vector3.forward) * qStart;
+
+		if (prevSnappedAngle != snappedAngle) {
+			float difference = snappedAngle - prevSnappedAngle;
+
+			if ( prevSnappedAngle < 0 && snappedAngle == 180-snapDegreesAtEvery) Debug.Log("Fixed right");
+			else if (prevSnappedAngle > 0 && snappedAngle == -180) Debug.Log("Fixed Left");
+			else if ( difference < 0) Debug.Log("right");
+			else Debug.Log("left");
+
+			Debug.Log("Snapped angle = " + snappedAngle);
+			prevSnappedAngle = snappedAngle;
+		}
+	}
+
+	float calculateSnap(float angle){
+		return Mathf.Floor(angle/snapDegreesAtEvery)*snapDegreesAtEvery;
+	}
+}
