464b9cb2b85105218899d69a6353e266d2d7b8ab
Jill <jillzyeung@gmail.com>
2015-01-25 01:49:01 -0800

    Added base classes for tetriminos and grid manager

---
+++ Assets/scripts/GridManagerScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GridManagerScript : MonoBehaviour {
+	
+	public GameObject squareTet, lineTet, hillTet, 
+	rightLTet, backwardLTet, rightZTet, backwardZTet;
+
+
+	GameObject [][] grid = new GameObject[24][];
+	public List <GameObject> sandbox = new List<GameObject>();
+	//make tetriminoCatalog array of possible tetriminos 
+	public List <GameObject> tetCatalog = new List<GameObject>();
+
+	//instantiate
+
+	void Start() {
+
+		tetCatalog.Add(squareTet);
+		tetCatalog.Add(lineTet);
+		tetCatalog.Add(hillTet);
+		tetCatalog.Add(rightLTet);
+		tetCatalog.Add(backwardLTet);
+		tetCatalog.Add(rightZTet);
+		tetCatalog.Add(backwardZTet);
+
+		// every 5 seconds, make a new tetrimino 
+		InvokeRepeating("SpawnTetrimino",0, 5.0f);
+	}
+
+	void Update() {
+		// checking for completed rows
+		// every second each block moves down 1 unit
+	}
+
+	private void SpawnTetrimino() {
+		//instantiate the randomly selected prefab
+		GameObject randomTetrimino = getRandomTet ();
+		Instantiate (randomTetrimino, Vector3.zero, Quaternion.identity);
+	}
+
+	//a getRandomTet funciton that returns a random prefab(tet)
+	private GameObject getRandomTet() {
+		return tetCatalog[Random.Range(0, tetCatalog.Count - 1)];
+	}
+
+	private int getRandomInt (int upperBound) {
+		return Random.Range(0, upperBound);		
+	}
+
+}
---
+++ Assets/scripts/Tet5Script.cs
@@ -0,0 +1,22 @@
+﻿//backwardL
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Tet5Script : MonoBehaviour {
+	
+	public GameObject blockPrefab;
+	public List <GameObject> blocks = new List<GameObject>();
+	
+	void Start () {
+		
+		GameObject newBlock1 = (Instantiate (blockPrefab, new Vector2 (0,0), Quaternion.identity)) as GameObject;
+		blocks.Add (newBlock1);
+		GameObject newBlock2 = (Instantiate (blockPrefab, new Vector2 (0,-1), Quaternion.identity)) as GameObject;
+		blocks.Add (newBlock2);
+		GameObject newBlock3 = (Instantiate (blockPrefab, new Vector2 (-1,-2), Quaternion.identity)) as GameObject;
+		blocks.Add (newBlock3);
+		GameObject newBlock4 = (Instantiate (blockPrefab, new Vector2 (0,-2), Quaternion.identity)) as GameObject;
+		blocks.Add (newBlock4);
+		
+	}
+}
