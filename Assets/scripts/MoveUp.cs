1210732611b79396a6ac3ba4d363fdd8cdc6d13d
fernando-carrillo <fernando.carrillo@onlive.com>
2015-01-24 16:05:59 -0800

    Adding initial skeleton files

---
+++ Assets/scripts/MoveUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveUp : MonoBehaviour {
+	public float xSpeed = .01f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x + xSpeed, transform.position.y);
+	}
+}
